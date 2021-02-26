0bce5e76f41bfe8437319bade21e06069e9d8444
Antonio Goncalves <amng835@gmail.com>
2021-02-26 20:47:36 +0000

    Changed python version required

--- setup.py
+++ setup.py
@@ -4,15 +4,24 @@ import pathlib
 README = (pathlib.Path(__file__).parent / "README.md").read_text(encoding='utf8')
 
 setup(
-    name='Pretty Downloader',
+    name='pretty-downloader',
     packages=find_packages(),
     version='0.0.4',
     description='This is a simple python pretty downloader for your projects.',
     long_description=README,
     long_description_content_type='text/markdown',
     author='DeadSec-Security',
-    autho_email='amng835@gmail.com',
+    author_email='amng835@gmail.com',
     url='https://github.com/DEADSEC-SECURITY/pretty-downloader',
+    keywords=[
+        'progressbar',
+        'downloader',
+        'download',
+        'console',
+        'bar',
+        'progress',
+        'download-bar'
+    ],
     license='MIT',
     install_requires=[
         'tqdm==4.57.0',
